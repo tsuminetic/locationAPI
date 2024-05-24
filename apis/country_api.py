47abff1d64b43e2d6de670fd9bbb6e1091f6e1ab
tsuminetic <diensanc@gmail.com>
2024-05-24 13:54:21 +0545

    country api

--- apis/country_api.py
+++ apis/country_api.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from app import db
-from models.country import Country, CountrySchema
+from models.country import Country, CountrySchema,CountryLoader
+from werkzeug.exceptions import BadRequest
 
 api = Blueprint('country_api', __name__)
 
@@ -16,13 +17,16 @@ def get_all():
 # Get a specific country
 @api.route('/<int:country_id>', methods=['GET'])
 def get_country(country_id):
-    country = Country.query.get(country_id)
-    if not country:
-        return jsonify({'error': 'resource not found'}), 404
+    try:
+        country = Country.query.get(country_id)
+        if not country:
+            raise BadRequest("resource not found")
 
-    country_schema = CountrySchema()
-    data = country_schema.dump(country)
-    return jsonify({"data": data})
+        country_schema = CountrySchema()
+        data = country_schema.dump(country)
+        return jsonify({"data": data})
+    except BadRequest as err: 
+        return jsonify({'error': str(err)}), 400
 
 # Add a country
 @api.route('', methods=['POST'])
@@ -31,49 +35,52 @@ def add_country():
 
     try:
         country_data = country_schema.load(request.get_json()) 
+        db.session.add(country_data)
+        db.session.commit()
+
+        response_data = country_schema.dump(country_data)
+        return jsonify({"data": response_data})
+
     except ValidationError as err:
         return jsonify({'error': err.messages}), 400
 
-    db.session.add(country_data)
-    db.session.commit()
-
-    response_data = country_schema.dump(country_data)
-    return jsonify({"data": response_data})
-
+    
 # Delete a country
 @api.route('/<int:country_id>', methods=['DELETE'])
 def delete_country(country_id):
-    country = Country.query.get(country_id)
-    if not country:
-        return jsonify({'error': 'resource not found'}), 404
-    
-    db.session.delete(country)
-    db.session.commit()
+    try:
+        country = Country.query.get(country_id)
+        if not country:
+            raise BadRequest("resource not found")
+        
+        db.session.delete(country)
+        db.session.commit()
 
-    return jsonify({'success': True})
+        return jsonify({'success': True})
+    except BadRequest as err: 
+        return jsonify({'error': str(err)}), 400
 
 # edit a country
 @api.route('/<int:country_id>', methods=['PUT'])
 def edit_country(country_id):
-    country = Country.query.get(country_id)
-    
-    if not country:
-        return jsonify({'error': 'Country not found'}), 404
-
-    country_schema = CountrySchema(partial=True)
-    
     try:
+        country = Country.query.get(country_id)
+        
+        if not country:
+            raise BadRequest("resource not found")
+
+        country_loader = CountryLoader(partial=True)
+        country_schema = CountrySchema()
+        
         request_data = request.get_json()
-        validated_data = country_schema.load(request_data, partial=True)
+        validated_data = country_loader.load(request_data,instance=country, partial=True)
+        
+        db.session.merge(validated_data)
+        db.session.commit()
+
+        updated_country_data = country_schema.dump(country)
+        return jsonify({"data": updated_country_data})
+    except BadRequest as err: 
+        return jsonify({'error': str(err)}), 400
     except ValidationError as err:
         return jsonify({'error': err.messages}), 400
-    
-    for key, value in request_data.items():
-        if key=="id":
-            return jsonify({'error': "not allowed"}), 403
-        setattr(country, key, value)
-
-    db.session.commit()
-
-    updated_country_data = country_schema.dump(country)
-    return jsonify({"data": updated_country_data})
