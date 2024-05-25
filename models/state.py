55f664fb4d2a8e2474a9508a7d846de5d7540a5d
tsuminetic <diensanc@gmail.com>
2024-05-25 21:39:46 +0545

    state model

--- models/state.py
+++ models/state.py
@@ -11,8 +11,10 @@ class State(db.Model):
     country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
     country= db.relationship("Country", backref=db.backref("states", cascade="all, delete-orphan"))
     
-class StateSchema(ma.SQLAlchemyAutoSchema):
     
+from models.city import CityNameSchema
+class StateSchema(ma.SQLAlchemyAutoSchema):
+    cities = ma.Nested(CityNameSchema, many=True)
     class Meta:
         model = State
         load_instance = True
@@ -23,7 +25,8 @@ class StateSchema(ma.SQLAlchemyAutoSchema):
             "timezone",
             "latitude",
             "longitude",
-            "country_id"
+            "country_id",
+            "cities"
         )
 class StateLoader(ma.SQLAlchemyAutoSchema):
     
