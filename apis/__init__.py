562534ffd628a2f9f499a511844bb4a236ea19f7
tsuminetic <diensanc@gmail.com>
2024-05-25 21:38:18 +0545

    api initialization

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,8 +1,8 @@
 from apis.country_api import api as country_api
 from apis.state_api import api as state_api
-# from api.city_api import api as city_api
+from apis.city_api import api as city_api
 
 def initialize_routes(app):
     app.register_blueprint(country_api, url_prefix='/countries')
     app.register_blueprint(state_api, url_prefix='/states')
-    # app.register_blueprint(city_api, url_prefix='/cities')
+    app.register_blueprint(city_api, url_prefix='/cities')
