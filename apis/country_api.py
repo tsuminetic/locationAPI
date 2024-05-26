968ed42025cb4a98be22833a8dff717ee56c7bbb
tsuminetic <diensanc@gmail.com>
2024-05-26 16:24:07 +0545

    country api cleaned

--- apis/country_api.py
+++ apis/country_api.py
@@ -17,70 +17,54 @@ def get_all():
 # Get a specific country
 @api.route('/<int:country_id>', methods=['GET'])
 def get_country(country_id):
-    try:
-        country = Country.query.get(country_id)
-        if not country:
-            raise BadRequest("resource not found")
+    country = Country.query.get(country_id)
+    if not country:
+        raise BadRequest("resource not found")
 
-        country_schema = CountrySchema()
-        data = country_schema.dump(country)
-        return jsonify({"data": data})
-    except BadRequest as err: 
-        return jsonify({'error': str(err)}), 400
+    country_schema = CountrySchema()
+    data = country_schema.dump(country)
+    return jsonify({"data": data})
 
 # Add a country
 @api.route('', methods=['POST'])
 def add_country():
     country_schema = CountrySchema()
+    
+    country_data = country_schema.load(request.get_json()) 
+    db.session.add(country_data)
+    db.session.commit()
 
-    try:
-        country_data = country_schema.load(request.get_json()) 
-        db.session.add(country_data)
-        db.session.commit()
-
-        response_data = country_schema.dump(country_data)
-        return jsonify({"data": response_data})
-
-    except ValidationError as err:
-        return jsonify({'error': err.messages}), 400
-
+    response_data = country_schema.dump(country_data)
+    return jsonify({"data": response_data})
     
 # Delete a country
 @api.route('/<int:country_id>', methods=['DELETE'])
 def delete_country(country_id):
-    try:
-        country = Country.query.get(country_id)
-        if not country:
-            raise BadRequest("resource not found")
-        
-        db.session.delete(country)
-        db.session.commit()
+    country = Country.query.get(country_id)
+    if not country:
+        raise BadRequest("resource not found")
+    
+    db.session.delete(country)
+    db.session.commit()
 
-        return jsonify({'success': True})
-    except BadRequest as err: 
-        return jsonify({'error': str(err)}), 400
+    return jsonify({'success': True})
 
 # edit a country
 @api.route('/<int:country_id>', methods=['PUT'])
 def edit_country(country_id):
-    try:
-        country = Country.query.get(country_id)
-        
-        if not country:
-            raise BadRequest("resource not found")
+    country = Country.query.get(country_id)
+    
+    if not country:
+        raise BadRequest("resource not found")
 
-        country_loader = CountryLoader(partial=True)
-        country_schema = CountrySchema()
-        
-        request_data = request.get_json()
-        validated_data = country_loader.load(request_data,instance=country, partial=True)
-        
-        db.session.merge(validated_data)
-        db.session.commit()
+    country_loader = CountryLoader(partial=True)
+    country_schema = CountrySchema()
+    
+    request_data = request.get_json()
+    validated_data = country_loader.load(request_data,instance=country, partial=True)
+    
+    db.session.merge(validated_data)
+    db.session.commit()
 
-        updated_country_data = country_schema.dump(country)
-        return jsonify({"data": updated_country_data})
-    except BadRequest as err: 
-        return jsonify({'error': str(err)}), 400
-    except ValidationError as err:
-        return jsonify({'error': err.messages}), 400
+    updated_country_data = country_schema.dump(country)
+    return jsonify({"data": updated_country_data})
