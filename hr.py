ee8325dd56e6f9ce149062da3ef3e1ec7bd84a8f
tsuminetic <diensanc@gmail.com>
2024-05-27 14:26:39 +0545

    hr

--- hr.py
+++ hr.py
@@ -1,7 +1,13 @@
 
 def climbingLeaderboard(ranked, player):
-    return 0
+    unique_ranks = sorted(set(ranked), reverse=True)
+    results = []
+    l = len(unique_ranks)
+    for score in player:
+        while l > 0 and score >= unique_ranks[l - 1]:
+            l -= 1
+        results.append(l + 1)
+    
+    return results
 
-
-
-climbingLeaderboard([100,100,50,40,40,20,10],[5,25,50,120])
+print(climbingLeaderboard([100,100,50,40,40,20,10],[5,25,50,120]))
