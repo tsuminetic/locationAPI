b27e4480be255e0b13c6b69e8948dc6dd174c2cd
tsuminetic <diensanc@gmail.com>
2024-05-29 15:50:44 +0545

    saveThePrisoner

--- hr.py
+++ hr.py
@@ -1,13 +1,4 @@
-import math
-
-def viralAdvertising(days):
-    toShare=5
-    totalLikers=0
-    for day in range(days):
-        likers=math.floor(toShare/2)
-        totalLikers+=likers
-        toShare = likers*3
-    return totalLikers
-
-
-print(viralAdvertising(5))
+def saveThePrisoner(prisoners, candies, startFrom):
+    return ((startFrom+candies-2)%prisoners)+1
+    
+print(saveThePrisoner(4,6,2))
