e186015dce255dd643adf45130a1c0a283bd5d38
tsuminetic <diensanc@gmail.com>
2024-05-29 10:44:35 +0545

    venv

--- hr.py
+++ hr.py
@@ -1,13 +1,7 @@
-
-def climbingLeaderboard(ranked, player):
-    unique_ranks = sorted(set(ranked), reverse=True)
-    results = []
-    l = len(unique_ranks)
-    for score in player:
-        while l > 0 and score >= unique_ranks[l - 1]:
-            l -= 1
-        results.append(l + 1)
-    
-    return results
-
-print(climbingLeaderboard([100,100,50,40,40,20,10],[5,25,50,120]))
+import requests
+resp = requests.post('https://textbelt.com/text', {
+  'phone': '9821898939',
+  'message': 'Hello world',
+  'key': 'textbelt',
+})
+print(resp.json())
