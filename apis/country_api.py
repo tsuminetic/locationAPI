50fa98f66e71201211364b5486b5dfc9c0ccf658
tsuminetic <diensanc@gmail.com>
2024-05-29 10:58:01 +0545

    country api (class)

--- apis/country_api.py
+++ apis/country_api.py
@@ -1,70 +1,83 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
+from flask.views import MethodView
 from app import db
-from models.country import Country, CountrySchema,CountryLoader
+from models.country import Country, CountrySchema, CountryLoader
 from werkzeug.exceptions import BadRequest
 
 api = Blueprint('country_api', __name__)
 
-# List all countries
-@api.route('', methods=['GET'])
-def get_all():
-    countries = Country.query.all()
-    country_schema = CountrySchema(many=True)
-    data = country_schema.dump(countries)
-    return jsonify({"data": data, "metadata": {"total": len(countries)}})
+class CountryAPI(MethodView):
 
-# Get a specific country
-@api.route('/<int:country_id>', methods=['GET'])
-def get_country(country_id):
-    country = Country.query.get(country_id)
-    if not country:
-        raise BadRequest("resource not found")
+    def get(self, country_id=None):
+        if country_id is None:
+            # List all countries
+            countries = Country.query.all()
+            country_schema = CountrySchema(many=True)
+            data = country_schema.dump(countries)
+            return jsonify({"data": data, "metadata": {"total": len(countries)}})
+        else:
+            # Get a specific country
+            country = Country.query.get(country_id)
+            if not country:
+                raise BadRequest("resource not found")
 
-    country_schema = CountrySchema()
-    data = country_schema.dump(country)
-    return jsonify({"data": data})
+            country_schema = CountrySchema()
+            data = country_schema.dump(country)
+            return jsonify({"data": data})
 
-# Add a country
-@api.route('', methods=['POST'])
-def add_country():
-    country_schema = CountrySchema()
-    
-    country_data = country_schema.load(request.get_json()) 
-    db.session.add(country_data)
-    db.session.commit()
+    def post(self):
+        # Add a country
+        country_schema = CountrySchema()
 
-    response_data = country_schema.dump(country_data)
-    return jsonify({"data": response_data})
-    
-# Delete a country
-@api.route('/<int:country_id>', methods=['DELETE'])
-def delete_country(country_id):
-    country = Country.query.get(country_id)
-    if not country:
-        raise BadRequest("resource not found")
-    
-    db.session.delete(country)
-    db.session.commit()
+        try:
+            country_data = country_schema.load(request.get_json())
+        except ValidationError as err:
+            raise BadRequest(err.messages)
+        
+        db.session.add(country_data)
+        db.session.commit()
 
-    return jsonify({'success': True})
+        response_data = country_schema.dump(country_data)
+        return jsonify({"data": response_data})
 
-# edit a country
-@api.route('/<int:country_id>', methods=['PUT'])
-def edit_country(country_id):
-    country = Country.query.get(country_id)
-    
-    if not country:
-        raise BadRequest("resource not found")
+    def delete(self, country_id):
+        # Delete a country
+        country = Country.query.get(country_id)
+        if not country:
+            raise BadRequest("resource not found")
+        
+        db.session.delete(country)
+        db.session.commit()
 
-    country_loader = CountryLoader(partial=True)
-    country_schema = CountrySchema()
-    
-    request_data = request.get_json()
-    validated_data = country_loader.load(request_data,instance=country, partial=True)
-    
-    db.session.merge(validated_data)
-    db.session.commit()
+        return jsonify({'success': True})
 
-    updated_country_data = country_schema.dump(country)
-    return jsonify({"data": updated_country_data})
+    def put(self, country_id):
+        # Edit a country
+        country = Country.query.get(country_id)
+        
+        if not country:
+            raise BadRequest("resource not found")
+
+        country_loader = CountryLoader(partial=True)
+        country_schema = CountrySchema()
+        
+        request_data = request.get_json()
+        try:
+            validated_data = country_loader.load(request_data, instance=country, partial=True)
+        except ValidationError as err:
+            raise BadRequest(err.messages)
+        
+        db.session.merge(validated_data)
+        db.session.commit()
+
+        updated_country_data = country_schema.dump(country)
+        return jsonify({"data": updated_country_data})
+
+# Register the views
+country_view = CountryAPI.as_view('country_api')
+
+# Add routes
+api.add_url_rule('', defaults={'country_id': None}, view_func=country_view, methods=['GET'])
+api.add_url_rule('', view_func=country_view, methods=['POST'])
+api.add_url_rule('/<int:country_id>', view_func=country_view, methods=['GET', 'PUT', 'DELETE'])
