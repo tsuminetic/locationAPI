73ed5f8f11ecd1b94decf77f88971483532ca36f
tsuminetic <diensanc@gmail.com>
2024-05-29 13:07:35 +0545

    viralAdvertising

--- hr.py
+++ hr.py
@@ -1,19 +1,13 @@
-def rev(number):
-    numberinlist = []
-    for digit in str(number):
-        numberinlist.append(digit)
-    numberinlist.reverse()
-    result = ""
-    for digit in numberinlist:
-        result+=digit
-    return int(result)
+import math
 
-def beautifulDays(startingDay, endingDay, divisor):
-    resultDays = 0
-    for day in range(startingDay,endingDay+1):
-        if ((day - rev(day))/divisor).is_integer():
-            resultDays+=1
-    return resultDays
-  
-print(beautifulDays(20, 23, 6))
+def viralAdvertising(days):
+    toShare=5
+    totalLikers=0
+    for day in range(days):
+        likers=math.floor(toShare/2)
+        totalLikers+=likers
+        toShare = likers*3
+    return totalLikers
 
+
+print(viralAdvertising(5))
