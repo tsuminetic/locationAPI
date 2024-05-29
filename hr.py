86e84b484d7a983fc884c39ad7979b545b8e9a12
tsuminetic <diensanc@gmail.com>
2024-05-29 12:53:52 +0545

    beautifuldays

--- hr.py
+++ hr.py
@@ -1,7 +1,19 @@
-import requests
-resp = requests.post('https://textbelt.com/text', {
-  'phone': '9821898939',
-  'message': 'Hello world',
-  'key': 'textbelt',
-})
-print(resp.json())
+def rev(number):
+    numberinlist = []
+    for digit in str(number):
+        numberinlist.append(digit)
+    numberinlist.reverse()
+    result = ""
+    for digit in numberinlist:
+        result+=digit
+    return int(result)
+
+def beautifulDays(startingDay, endingDay, divisor):
+    resultDays = 0
+    for day in range(startingDay,endingDay+1):
+        if ((day - rev(day))/divisor).is_integer():
+            resultDays+=1
+    return resultDays
+  
+print(beautifulDays(20, 23, 6))
+
