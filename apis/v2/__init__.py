dd9e9274900850acca9ed6451117e8efac05c24a
tsuminetic <diensanc@gmail.com>
2024-06-03 13:26:53 +0545

    v2 api init

--- apis/v2/__init__.py
+++ apis/v2/__init__.py
@@ -1,4 +1,8 @@
-from apis.v2.resourse_api import api as ResourceAPI
+from apis.v2.city_api import api as city_api_v2
+from apis.v2.state_api import api as state_api_v2
+from apis.v2.country_api import api as country_api_v2
 
 def initialize_routes_v2(app):
-    app.register_blueprint(ResourceAPI, url_prefix='/v2')
+    app.register_blueprint(city_api_v2, url_prefix='/v2')
+    app.register_blueprint(state_api_v2, url_prefix='/v2')
+    app.register_blueprint(country_api_v2, url_prefix='/v2')
