783e62db495c30e0c1bb26a633f35b897dce2754
tsuminetic <diensanc@gmail.com>
2024-06-10 12:01:16 +0545

    workig search route for state

--- apis/v2/state_api.py
+++ apis/v2/state_api.py
@@ -18,6 +18,39 @@ class StateAPI(ResourceAPI):
             raise BadRequest('country does not exist')
         return body
 
+    def filter_query(self, query, query_params):
+        filters = {
+            'name': lambda value: self.model_class.name.like(f"%{value}%"),
+            'population_min': lambda value: self.model_class.population >= int(value),
+            'population_max': lambda value: self.model_class.population <= int(value),
+        }
+
+        for param, value in query_params.items():
+            if param in filters:
+                query = query.filter(filters[param](value))
+            elif param == 'country_name':
+                query = query.join(Country).filter(Country.name.like(f"%{value}%"))
+                
+        return query
+    
+    def order_query(self, query, query_params):
+        order_by_mappings = {
+            'name': self.model_class.name,
+            'population': self.model_class.population,
+            'country_id': self.model_class.country_id,
+            'id': self.model_class.id
+        }
+
+        order_by = query_params.get('order_by')
+        order_direction = query_params.get('order_direction', 'asc')  
+
+        if order_by in order_by_mappings:
+            field = order_by_mappings[order_by]
+            if order_direction == 'desc':
+                field = field.desc()
+            query = query.order_by(field)
+
+        return query
 # Register the views
 state_view = StateAPI.as_view('state_api_v2')
 
@@ -25,3 +58,4 @@ state_view = StateAPI.as_view('state_api_v2')
 api.add_url_rule('/states', defaults={'resource_id': None}, view_func=state_view, methods=['GET'])
 api.add_url_rule('/states', view_func=state_view, methods=['POST'])
 api.add_url_rule('/states/<int:resource_id>', view_func=state_view, methods=['GET', 'PUT', 'DELETE'])
+api.add_url_rule('/states/search', view_func=state_view, methods=['GET'], endpoint='search')
